8d120b98a12247739f54629afa7a3a424c95c803
Aguinele Queiroz <aguinelequeiroz@outlook.com>
2022-12-27 21:15:51 -0300

    Revert "update features"
    This reverts commit 153240dbf50fc20495be4884f4e7476224722bba.

--- assets/js/decoder.js
+++ assets/js/decoder.js
@@ -14,7 +14,6 @@ function hashCodeForEncripty(element) {
         'u': '3#$91ç1l',
     }
     if (codesForEncripty.includes(element)) return codesForEncripty[element];
-    return;
 }
 // substitui os caracteres por um hashcode
 function replaceCharWithHashCode(element) {
@@ -24,25 +23,27 @@ function replaceCharWithHashCode(element) {
             break;
         case 'e':
             strArr[element] = hashCodeForEncripty(element);
-            break;
         case 'i':
             strArr[element] = hashCodeForEncripty(element);
-            break;
         case 'o':
             strArr[element] = hashCodeForEncripty(element);
-            break;
         case 'u':
             strArr[element] = hashCodeForEncripty(element);
-            break;
         default:
-            return strArr;
+            return;
     }
-    return strArr;
 }
 // criptografa a string passada como argumento 
-function encriptyString(strArr) {
+const encriptyString = (strArr) => {
     strArr.forEach(element => {
         replaceCharWithHashCode(element);
     });
     return strArr;
 }
+
+// lógica de captura das entradas na textarea
+/* 
+*
+*   ...
+*
+*/
