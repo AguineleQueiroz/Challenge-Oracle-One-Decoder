bdd6aa67ba557aef74310b294605940fee6862df
Aguinele Queiroz <aguinelequeiroz@outlook.com>
2022-12-30 12:21:01 -0300

    update hash object

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,23 @@
 *
 */
 
+/*
 const CODES_FOR_ENCRYPT = {
     'a': '1#$24nI/Gg%*',
     'e': '$Hg%nI)',
     'i': '4nIwG%Ff',
     'o': 'nI1#$24g%',
     'u': '3#$91ç1l',
+    ' ': 'ffx'
+}
+*/
+
+const CODES_FOR_ENCRYPT = {
+    'a': 'ai',
+    'e': 'enter',
+    'i': 'imes',
+    'o': 'ober',
+    'u': 'ufat',
     ' ': 'ffx'
 }
 
