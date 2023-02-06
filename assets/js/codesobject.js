c962543515fd1e800c48685ab4527d32b366d533
Aguinele Queiroz <aguinelequeiroz@outlook.com>
2023-02-06 15:16:59 -0300

    update dummy hash object

--- assets/js/codesobject.js
+++ assets/js/codesobject.js
@@ -1,20 +1,10 @@
 export { CODES_FOR_ENCRYPT };
-/* 
+
 const CODES_FOR_ENCRYPT = {
     'a': '1#$24nI/Gg%*',
     'e': '$Hg%nI)',
     'i': '4nIwG%Ff',
     'o': 'nI1#$24g%',
     'u': '3#$91ç1l',
-    ' ': 'ffx'
-}
-*/
-
-const CODES_FOR_ENCRYPT = {
-    'a': 'ai',
-    'e': 'enter',
-    'i': 'imes',
-    'o': 'ober',
-    'u': 'ufat',
     ' ': 'ffx'
 }
